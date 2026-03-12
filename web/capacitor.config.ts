926a287e851204d78954a94601ca1835f850b7e2
Marlon Harrison <92547717+marlon-ionic@users.noreply.github.com>
2026-03-11 23:43:22 -0500

    chore(deps): cap 7, portals@0.12.0

--- web/capacitor.config.ts
+++ web/capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.portals.ecommercewebapp',
   appName: 'Portals Web App',
-  webDir: 'build',
-  bundledWebRuntime: false
+  webDir: 'build'
 };
 
 export default config;
