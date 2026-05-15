40f90423c35e584859f5f5fd9ec9e999877d1daf
Ryan Twohey <rtwohey@ionic.io>
2026-05-15 12:04:08 -0400

    updated text to test live update 2

--- web/src/pages/CheckoutPage/CheckoutPage.tsx
+++ web/src/pages/CheckoutPage/CheckoutPage.tsx
@@ -119,7 +119,7 @@ const CheckoutPage: React.FC = () => {
                   Portals.publish({ topic: 'dismiss', data: result });
                 }}
               >
-                Place Your Order
+                Place Your Order Here
               </IonButton>
             </>
           )}
