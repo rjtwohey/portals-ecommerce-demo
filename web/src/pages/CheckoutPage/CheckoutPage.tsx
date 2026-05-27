7e6832eb04eaa0871013735c636133922c2435e3
Ryan Twohey <rtwohey@ionic.io>
2026-05-27 08:31:14 -0400

    Fixed Button Text (Live Update Test)

--- web/src/pages/CheckoutPage/CheckoutPage.tsx
+++ web/src/pages/CheckoutPage/CheckoutPage.tsx
@@ -119,7 +119,7 @@ const CheckoutPage: React.FC = () => {
                   Portals.publish({ topic: 'dismiss', data: result });
                 }}
               >
-                Place Your Order Please
+                Place Your Order
               </IonButton>
             </>
           )}
